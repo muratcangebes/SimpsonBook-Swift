029b3d5623e9a8b7111335062931de7c1af517ab
Muratcan Gebeş <muratcan.gebes19@gmail.com>
2025-03-04 19:45:34 +0300

    Add files via upload

---
+++ SimpsonBook/SimpsonBook/Simpson.swift
@@ -0,0 +1,23 @@
+//
+//  Simpson.swift
+//  SimpsonBook
+//
+//  Created by Muratcan Gebeş on 4.03.2025.
+//
+
+import Foundation
+import UIKit
+
+class Simpson{
+    
+    var name : String
+    var job : String
+    var image : UIImage
+    
+    init(simpsonName: String, simpsonJob: String, simpsonImage: UIImage) {
+        name = simpsonName
+        job = simpsonJob
+        image = simpsonImage
+    }
+    
+}
---
+++ SimpsonBook/SimpsonBook/detailsVC.swift
@@ -0,0 +1,31 @@
+//
+//  detailsVC.swift
+//  SimpsonBook
+//
+//  Created by Muratcan Gebeş on 4.03.2025.
+//
+
+import UIKit
+
+class detailsVC: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var jobLabel: UILabel!
+    
+    var selectedSimpson : Simpson?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        nameLabel.text = selectedSimpson?.name
+        jobLabel.text = selectedSimpson?.job
+        imageView.image = selectedSimpson?.image
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+}
